b9444a272aa6db1113214810c29a6092e7ded22f
Yair Sadan <yairsadan1@gmail.com>
2023-11-23 16:46:10 +0200

    Add fonts and update colors in tailwind.config.ts

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Rubik } from "next/font/google";
+import { Overpass } from "next/font/google";
+
+export const rubik = Rubik({ subsets: ['hebrew'] });
+export const overpass = Overpass({ subsets: ['latin']});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,21 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'primary': '#808B77',
+      'secondary': '#DECCBC',
+      'tertiary': '#451827',
+      'tertiary-900': '#451827E5',
+      'tertiary-700': '#451827B2',
+      'accent': '#14161B80',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
