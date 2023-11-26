ef686e67ce80d9a644958755a470c0184569737f
Eliezer Perl <eliezerrules@gmail.com>
2023-11-26 13:31:59 +0200

    login page ui fix

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -3,19 +3,15 @@ import React from "react";
 const LoginForm = () => {
 	return (
 		<>
-			<input type="text" />
-			<button>שלח קוד אימות</button>
+			<input
+				type="text"
+				placeholder="טלפון:"
+				className="btn-primary placeholder-tertiary-700"
+			/>
+			<button type="submit" className="btn-primary">
+				שלח קוד אימות
+			</button>
 		</>
-		// <div className="flex flex-col gap-12 p-24 py-48">
-		// 	<input
-		// 		type="text"
-		// 		placeholder="טלפון:"
-		// 		className="btn-primary placeholder-tertiary-700"
-		// 	/>
-		// 	<button type="submit" className="btn-primary">
-		// 		<div>שלח קוד אימות</div>
-		// 	</button>
-		// </div>
 	);
 };
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,10 @@ import LoginForm from "./components/LoginForm";
 
 const Login = () => {
 	return (
-		<main className="page-primary p-24">
-			<h1 className="title-primary">ברוכים הבאים</h1>
-			<div className="w">
+		<main className="page-primary justify-center">
+			<h1 className="title-primary pb-60">ברוכים הבאים</h1>
+
+			<div className="flex flex-col items-center gap-6">
 				<LoginForm />
 			</div>
 		</main>
