a80867301298d3fe9ef01b1eee07221626cb0f26
Eliezer Perl <eliezerrules@gmail.com>
2023-11-27 11:43:55 +0200

    admin hompage

---
+++ src/app/admin/components/AdminOptions.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Button from "@/app/ui/globalComponents/Button";
+import React from "react";
+
+const AdminOptions = () => {
+	return (
+		<>
+			<Button text="להוסיף לקוח" onClick={() => {}} />
+			<Button text="לערוך לוח זמנים" onClick={() => {}} />
+			<Button text="לנהל תשלומים" onClick={() => {}} />
+			<Button text="הודעות" onClick={() => {}} />
+		</>
+	);
+};
+
+export default AdminOptions;
--- src/app/user/components/Options.tsx
+++ src/app/user/components/Options.tsx
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 import Button from "@/app/ui/globalComponents/Button";
 import React from "react";
 
 const Options = () => {
 	return (
 		<>
-			<Button text="לקבוע/לבטל תור" onClick={()=> {}}/>
-			<Button text="לשלם" onClick={()=> {}}/>
-			<Button text="לערוך פרטים אישיים" onClick={()=> {}}/>
+			<Button text="לקבוע/לבטל תור" onClick={() => {}} />
+			<Button text="לשלם" onClick={() => {}} />
+			<Button text="לערוך פרטים אישיים" onClick={() => {}} />
 		</>
 	);
 };
