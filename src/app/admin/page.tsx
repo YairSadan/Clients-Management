62bb8bad02231eaf286ca0987506fb012d9818d1
Eliezer Perl <eliezerrules@gmail.com>
2023-11-27 13:11:55 +0200

    eli init

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,18 +4,19 @@ import LogoutBtn from "../ui/globalComponents/LogoutBtn";
 import AdminOptions from "./components/AdminOptions";
 
 const AdminHome = () => {
-	return (
-		<main className="page-primary justify-around">
-			<div className="flex flex-row justify-start w-full">
-				<Title className="pr-8" text="שלום מנהל" />
-			</div>
-			<div className="flex flex-col items-center gap-3">
-				<Title className="pb-2" text="מה תרצה לעשות?" />
-				<AdminOptions />
-			</div>
-			<LogoutBtn />
-		</main>
-	);
+  return (
+    <main className="page-primary justify-around">
+      <div className="flex flex-row justify-start w-full">
+        <Title className="pr-8" text="שלום מנהל" />
+        HI FROM ELI
+      </div>
+      <div className="flex flex-col items-center gap-3">
+        <Title className="pb-2" text="מה תרצה לעשות?" />
+        <AdminOptions />
+      </div>
+      <LogoutBtn />
+    </main>
+  );
 };
 
 export default AdminHome;
