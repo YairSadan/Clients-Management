bdb4a20224be358a340a1396e1c661adb5f3dbd0
Yair Sadan <yairsadan1@gmail.com>
2023-12-04 13:42:40 +0200

    Add links to user options

--- src/app/user/components/Options.tsx
+++ src/app/user/components/Options.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
@@ -8,20 +9,14 @@ const Options = () => {
 
   return (
     <>
-      <Button
-        size={'optionButton'} asChild>
-        <Link href='/user/appointments'>
-        </Link>לקבוע/לבטל תור
+      <Button size={'optionButton'} asChild>
+        <Link href="/user/appointments">לקבוע/לבטל תור</Link>
       </Button>
-      <Button
-        size={'optionButton'}
-        onClick={() => {
-          router.push('/user/payments');
-        }}>
-        לשלם
+      <Button size={'optionButton'} asChild>
+        <Link href="/user/payments">לשלם</Link>
       </Button>
-      <Button size={'optionButton'} onClick={() => {}}>
-        לערוך פרטים אישיים
+      <Button size={'optionButton'} asChild>
+        <Link href="/user/profile">פרופיל</Link>
       </Button>
     </>
   );
