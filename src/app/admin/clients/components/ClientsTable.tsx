153f2b781aab79af384d858b2f11a36617cb97f6
Yair Sadan <yairsadan1@gmail.com>
2023-12-06 10:28:27 +0200

    Update "No results" message in ClientsTable component

--- src/app/admin/clients/components/ClientsTable.tsx
+++ src/app/admin/clients/components/ClientsTable.tsx
@@ -69,7 +69,7 @@ export default function DataTable<TData, TValue>({
           ) : (
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
+                אין נתונים
               </TableCell>
             </TableRow>
           )}
