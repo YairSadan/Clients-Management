db16a8344051cd80ab4f13ddd57118379dab6337
Yair Sadan <yairsadan1@gmail.com>
2023-12-07 15:14:02 +0200

    Add Luxon and React Big Calendar dependencies

--- src/app/admin/schedule/page.tsx
+++ src/app/admin/schedule/page.tsx
@@ -1,9 +1,26 @@
-'use client'
 import React from 'react';
-import Calendar from './components/Calendar';
-
-const AdminSchedule = () => {
-  return <Calendar />;
+import ScheduleCalendar from './components/ScheduleCalendar';
+import { Appointment } from '@prisma/client';
+import prisma from '@/lib/prisma';
+const adminSchedulePage = async () => {
+  'use server';
+  const appointments: Appointment[] = await prisma.appointment.findMany();
+  const addAppointment = async (appointment: Appointment) => {
+    'use server';
+    await prisma.appointment.create({
+      data: {
+        start: appointment.start,
+        end: appointment.end,
+        userId: 'clps6ex3l000nmx8rfala70as',
+      },
+    });
+  };
+  return (
+    <ScheduleCalendar
+      dbAppointments={appointments}
+      addAppointment={addAppointment}
+    />
+  );
 };
 
-export default AdminSchedule;
+export default adminSchedulePage;
