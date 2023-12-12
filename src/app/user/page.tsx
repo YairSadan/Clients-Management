8a10cb316de75ac537ec65eca78f09ae70bb1622
Yair Sadan <yairsadan1@gmail.com>
2023-12-12 21:34:01 +0200

    fix: a previous merge deleted the clients page and the options compnent

---
+++ src/app/user/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import LogoutBtn from '../ui/globalComponents/LogoutBtn';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+import Options from './components/Options';
+
+const ClientHome = async () => {
+  const session = await getServerSession(authOptions);
+  return (
+    <main className="page-primary justify-around">
+      <div className="flex flex-row justify-start w-full pr-8">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          שלום {session?.user.name}
+        </h2>
+      </div>
+      <div className="flex flex-col items-center gap-3">
+        <h3 className="pb-2 scroll-m-20 text-2xl font-semibold tracking-tight">
+          מה תרצה לעשות?
+        </h3>
+        <Options />
+      </div>
+      <LogoutBtn />
+    </main>
+  );
+};
+
+export default ClientHome;
