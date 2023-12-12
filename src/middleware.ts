3eb9629a2d78ba578239deba41ecce8645314abe
Yair Sadan <yairsadan1@gmail.com>
2023-12-12 21:06:16 +0200

    Update database configuration to use postgre on vercel and middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,20 +4,22 @@ import { NextResponse } from 'next/server';
 
 export default withAuth(
   function middleware(req: NextRequestWithAuth) {
+    if (req.nextUrl.pathname === '/') {
+      if (req.nextauth.token?.role === Role.ADMIN)
+        return NextResponse.redirect(new URL('/admin', req.url));
+      else if (req.nextauth.token?.role === Role.CLIENT)
+        return NextResponse.redirect(new URL('/user', req.url));
+    }
     if (
       req.nextUrl.pathname.startsWith('/admin') &&
       req.nextauth.token?.role !== Role.ADMIN
     )
       return NextResponse.redirect(new URL('/user', req.url));
-    
-    else if (
+    if (
       req.nextUrl.pathname.startsWith('/user') &&
       req.nextauth.token?.role !== Role.CLIENT
     )
       return NextResponse.redirect(new URL('/admin', req.url));
-    
-    else if (req.nextUrl.pathname === '/' || !req.nextUrl.pathname.startsWith('/admin' || '/user' || '/login'))
-      return NextResponse.redirect(new URL('/user', req.url));
   },
   {
     pages: {
